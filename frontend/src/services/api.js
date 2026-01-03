0cb8f68c0d31ac3fe59ebfb010d5810b0924e1e5
Poorvaja-Sooraj <poorvajasooraj264@gmail.com>
2026-01-03 19:45:00 +0530

    Vercel deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tradetrack-backend-x5mw.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
