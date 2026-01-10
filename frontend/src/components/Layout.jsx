032b92cf48b52dc6a65ddddfea5258de0735c6fc
Poorvaja-Sooraj <poorvajasooraj264@gmail.com>
2026-01-10 15:07:09 +0530

    Fix layout scaling using max-width container

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import { AuthContext } from "../context/auth/AuthContext";
-import { NavLink } from "react-router-dom";
 
 const Layout = ({ children }) => {
   const { setIsAuthenticated } = useContext(AuthContext);
@@ -14,27 +13,26 @@ const Layout = ({ children }) => {
   };
 
   return (
-    <div style={{ padding: "20px" }}>
+    <div className="app-container">
       <h1>TradeTrack</h1>
 
       <nav className="app-navbar">
-  <NavLink to="/products" className="nav-link">
-    Products
-  </NavLink>
+        <NavLink to="/products" className="nav-link">
+          Products
+        </NavLink>
 
-  <NavLink to="/billing" className="nav-link">
-    Billing
-  </NavLink>
+        <NavLink to="/billing" className="nav-link">
+          Billing
+        </NavLink>
 
-  <NavLink to="/bills" className="nav-link">
-    Bill History
-  </NavLink>
+        <NavLink to="/bills" className="nav-link">
+          Bill History
+        </NavLink>
 
-  <button className="nav-link logout-btn" onClick={handleLogout}>
-    Logout
-  </button>
-</nav>
-
+        <button className="nav-link logout-btn" onClick={handleLogout}>
+          Logout
+        </button>
+      </nav>
 
       {children}
     </div>
